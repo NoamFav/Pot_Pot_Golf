eb605b0344cc42b01d94320b8d8990ded23b13d0
bruna <b.cavalcantilauro@student.maastrichtuniversity.nl>
2024-03-19 19:26:23 +0100

    bruna

--- src/main/java/com/example/um_project_golf/EulerSolver.java
+++ src/main/java/com/example/um_project_golf/EulerSolver.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class EulerSolver {
 
     // Euler's method for solving systems of first-order differential equations
-    public static HashMap<String, Double> eulerMethod(List<List<InputManagement.Token>> derivatives, HashMap<String, Double> initialValues, double stepSize, double tInitial, double tFinal)
+    public static HashMap<String, Double> eulerMethod(List<List<InputManagement.Token>> derivatives, HashMap<String, Double> initialValues, double stepSize, double tInitial, double tFinal, List<String> equations)
     {
         int numSteps = (int) Math.ceil((tFinal-tInitial) / stepSize);// Number of steps necessary to get to the final time (Math.ceil just rounds up the number to an integer)
 
@@ -32,13 +32,13 @@ public class EulerSolver {
             // Update values using Euler method
             for (String variableName : values.keySet()) {
                 if (variableName.equals("t")) {
-                    values.put(variableName, t); // Makes sure the time is updated in each iteration
+                    values.put(variableName, t + stepSize); // Makes sure the time is updated in each iteration
                 } else {
                     values.put(variableName, values.get(variableName) + temporaryValues.get(variableName) * stepSize); // Updates the variables
                 }
             }
 
-            derivatives = inputManagement.constructFunctions(equations, values); // Update functions with the updated values
+            derivatives = inputManagement.constructCompleteFunctions(equations, values); // Update functions with the updated values
 
             // Update time
             t += stepSize;
@@ -48,37 +48,6 @@ public class EulerSolver {
     }
 
     static InputManagement inputManagement = new InputManagement(); // Initializes the input management
-    static List<String> equations = List.of("2t","t + 2y"); // Initializes the equations
-    static HashMap<String, Double> variables = new HashMap<>(); // Initializes the variables
 
-    public static void main(String[] args)
-    {
-        // Step size, initial time and final time
-        double stepSize = 0.1;
-        double tInitial = 2.0;
-        double tFinal = 2.5;
 
-        // Placeholders for each variable
-        variables.put("t", tInitial);
-        //variables.put("a", 1.0);
-        //variables.put("b", 3.0);
-        variables.put("x", 1.0);
-        variables.put("y", 3.0);
-        //variables.put("x", 0.0);
-        //variables.put("x", 0.0);
-        //variables.put("x", 0.0);
-
-        // Initialize functions with variables
-        List<List<InputManagement.Token>> functions = inputManagement.constructFunctions(equations, variables);
-
-        // Solve using Euler method
-        HashMap<String, Double> solutions = eulerMethod(functions, variables, stepSize, tInitial, tFinal);
-        solutions.remove("t",solutions.get("t"));
-
-        // Print the solution
-        for (String solution : solutions.keySet()) {
-            Double value = solutions.get(solution);
-            System.out.println("The value of " + solution + " at t = " + tFinal + " is: " + value);
-        }
-    }
 }
---
+++ src/main/java/com/example/um_project_golf/Solver.java
@@ -0,0 +1,50 @@
+package com.example.um_project_golf;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solver {
+    static InputManagement inputManagement = new InputManagement(); // Initializes the input management
+    static HashMap<String, Double> variables = new HashMap<>(); // Initializes the variables
+    static EulerSolver eulerSolver = new EulerSolver();
+    static ImprovedEuler improvedEuler = new ImprovedEuler();
+    static List<String> equations = List.of("2t","t + 2y"); // Initializes the equations
+
+    public static void main(String[] args)
+    {
+        // Step size, initial time and final time
+        double stepSize = 0.1;
+        double tInitial = 2.0;
+        double tFinal = 2.5;
+
+        // Placeholders for each variable
+        variables.put("t", tInitial);
+        variables.put("x", 1.0);
+        variables.put("y", 2.0);
+
+        // Initialize functions with variables
+        List<List<InputManagement.Token>> functions = inputManagement.constructCompleteFunctions(equations, variables);
+
+        // Solve using Euler's method
+        HashMap<String, Double> solutionsEuler = eulerSolver.eulerMethod(functions, variables, stepSize, tInitial, tFinal, equations);
+        solutionsEuler.remove("t",solutionsEuler.get("t"));
+
+        // Solve using Improved Euler's method
+        HashMap<String, Double> solutionsImprovedEuler = improvedEuler.improvedEulerMethod(functions, variables, stepSize, tInitial, tFinal, equations);
+        solutionsImprovedEuler.remove("t",solutionsImprovedEuler.get("t"));
+
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(7);
+
+        // Print the solutions
+        for (String solution : solutionsEuler.keySet()) {
+            Double valueE = solutionsEuler.get(solution);
+            Double valueIE = solutionsImprovedEuler.get(solution);
+            System.out.println("Euler's method:");
+            System.out.println("The value of " + solution + " at t = " + tFinal + " is: " + df.format(valueE));
+            System.out.println("Improved Euler's method:");
+            System.out.println("The value of " + solution + " at t = " + tFinal + " is: " + df.format(valueIE));
+        }
+    }
+}
