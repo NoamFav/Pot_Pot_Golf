3bc4e48546eb7d5b1c9698b545b76f73f9b5ee51
NoamFav <95364866+NoamFav@users.noreply.github.com>
2024-04-18 23:11:51 +0200

    Bug with reused vertices in obj files 🚧

--- target/classes/shaders/fragment.glsl
+++ target/classes/shaders/fragment.glsl
@@ -1,12 +1,42 @@
 #version 400 core
 
 in vec2 fragTextureCoord;
+in vec3 fragNormal;
+in vec3 fragPos;
 
 out vec4 fragColor;
 
+struct Material {
+    vec4 ambient;
+    vec4 diffuse;
+    vec4 specular;
+    int hasTexture;
+    float reflectance;
+};
+
 uniform sampler2D textureSampler;
+uniform vec3 ambientLight;
+uniform Material material;
+
+vec4 ambientC;
+vec4 diffuseC;
+vec4 specularC;
+
+void setupColor(Material material, vec2 textCoords) {
+    if (material.hasTexture == 1) {
+        ambientC = material.ambient * texture(textureSampler, textCoords);
+        diffuseC = ambientC;
+        specularC = ambientC;
+    } else {
+        ambientC = material.ambient;
+        diffuseC = material.diffuse;
+        specularC = material.specular;
+    }
+}
 
 void main()
 {
-    fragColor = texture(textureSampler, fragTextureCoord);
+    setupColor(material, fragTextureCoord);
+
+    fragColor = ambientC * vec4(ambientLight, 1);
 }
