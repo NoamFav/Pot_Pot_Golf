0d778fc84f3a89a536a1a326e8e17c8be96e4a38
NoamFav <95364866+NoamFav@users.noreply.github.com>
2024-04-20 20:52:10 +0200

    Added SpotLight 🔦

--- target/classes/shaders/fragment.glsl
+++ target/classes/shaders/fragment.glsl
@@ -29,12 +29,19 @@ struct PointLight {
     float exponent;
 };
 
+struct SpotLight {
+    PointLight pointLight;
+    vec3 coneDirection;
+    float cutoff;
+};
+
 uniform sampler2D textureSampler;
 uniform vec3 ambientLight;
 uniform Material material;
 uniform float specularPower;
 uniform DirectionalLight directionalLight;
 uniform PointLight pointLight;
+uniform SpotLight spotLight;
 
 vec4 ambientC;
 vec4 diffuseC;
@@ -77,6 +84,21 @@ vec4 calcPointLight(PointLight light, vec3 position, vec3 normal) {
     return ligh_color / attenuationInv;
 }
 
+vec4 calcSpotLight(SpotLight light, vec3 position, vec3 normal) {
+    vec3 to_light_dir = normalize(light.pointLight.position - position);
+    vec3 from_light_dir = -to_light_dir;
+    float spot_alpha = dot(from_light_dir, normalize(light.coneDirection));
+
+    vec4 color = vec4(0, 0, 0, 0);
+
+    if (spot_alpha > light.cutoff) {
+        color = calcPointLight(light.pointLight, position, normal);
+        color *= (1 - (1 - spot_alpha) / (1 - light.cutoff));
+    }
+
+    return color;
+}
+
 vec4 calcDirectionalLight(DirectionalLight light, vec3 position, vec3 normal) {
     return calcLightColor(light.color, light.intensity, position, normalize(light.direction), normal);
 }
@@ -87,6 +109,7 @@ void main()
 
     vec4 diffuseSpecularComp = calcDirectionalLight(directionalLight, fragPos, fragNormal);
     diffuseSpecularComp += calcPointLight(pointLight, fragPos, fragNormal);
+    diffuseSpecularComp += calcSpotLight(spotLight, fragPos, fragNormal);
 
     fragColor = ambientC * vec4(ambientLight, 1) + diffuseSpecularComp;
 }
