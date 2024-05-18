3663e08d066cba10df262e270d74960c0aecba46
bruna <b.cavalcantilauro@student.maastrichtuniversity.nl>
2024-05-18 18:19:31 +0200

    rule-base bot start

---
+++ src/main/java/com/um_project_golf/RuleBasedBot/Green.java
@@ -0,0 +1,33 @@
+package com.um_project_golf.RuleBasedBot;
+
+import java.util.function.Function;
+
+class Green {
+    private Function<Double, Double> heightProfile;
+    private Obstacle[] obstacles;
+    private double flagPositionX;
+    private double flagPositionY;
+    private double flagRadius;
+
+    // Constructor
+    public Green(Function<Double, Double> heightProfile, Obstacle[] obstacles, double flagPositionX, double flagPositionY, double flagRadius) {
+        this.heightProfile = heightProfile;
+        this.obstacles = obstacles;
+        this.flagPositionX = flagPositionX;
+        this.flagPositionY = flagPositionY;
+        this.flagRadius = flagRadius;
+    }
+
+    // Getters
+    public double getFlagPositionX() {
+        return flagPositionX;
+    }
+
+    public double getFlagPositionY() {
+        return flagPositionY;
+    }
+
+    public double getFlagRadius() {
+        return flagRadius;
+    }
+}
---
+++ src/main/java/com/um_project_golf/RuleBasedBot/Shot.java
@@ -0,0 +1,26 @@
+package com.um_project_golf.RuleBasedBot;
+
+class Shot {
+    private double velocityX;
+    private double velocityY;
+
+    // Constructor
+    public Shot(double velocityX, double velocityY) {
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+    }
+
+    // Getters
+    public double getVelocityX() {
+        return velocityX;
+    }
+
+    public double getVelocityY() {
+        return velocityY;
+    }
+
+    @Override
+    public String toString() {
+        return "VelocityX: " + velocityX + ", VelocityY: " + velocityY;
+    }
+}
