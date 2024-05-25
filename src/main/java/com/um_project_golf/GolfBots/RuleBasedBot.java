4d1fceed87b8f5b9742def459766c344b08a7c80
bruna <b.cavalcantilauro@student.maastrichtuniversity.nl>
2024-05-25 14:29:46 +0200

    changes in the bot logic

--- src/main/java/com/um_project_golf/GolfBots/RuleBasedBot.java
+++ src/main/java/com/um_project_golf/GolfBots/RuleBasedBot.java
@@ -1,39 +1,82 @@
 package com.um_project_golf.GolfBots;
 
+import com.um_project_golf.Core.Entity.SceneManager;
 import com.um_project_golf.Core.Entity.Terrain.HeightMap;
 import com.um_project_golf.Core.PhysicsEngine;
+import com.um_project_golf.Core.Utils.Consts;
 import org.joml.Vector3f;
 
-import java.util.function.Function;
-
 public class RuleBasedBot {
-    private static Vector3f startingPosition;
-    private static double flagRadius = 1;
-    private static Vector3f flagPosition;
-    private static HeightMap testMap = new HeightMap();
+    private Vector3f startingPosition;
+    private double flagRadius = 1;
+    private Vector3f flagPosition;
+    private HeightMap testMap;
 
-    public RuleBasedBot(Vector3f startingPosition, Vector3f flagPosition){
-        RuleBasedBot.startingPosition = startingPosition;
-        RuleBasedBot.flagPosition = flagPosition;
+
+    public RuleBasedBot(Vector3f startingPosition, Vector3f flagPosition, HeightMap testMap){
+        this.startingPosition = startingPosition;
+        this.flagPosition = flagPosition;
+        this.testMap = testMap;
     }
 
     public static void main(String[] args) {
+        HeightMap testMap = new HeightMap();
         testMap.createHeightMap();
-        float yInitial = testMap.getHeight(new Vector3f(1,0,0));
-        float yFlag = testMap.getHeight(new Vector3f(4,0,1));
+        float yInitial = testMap.getHeight(new Vector3f(1, 0, 0));
+        float yFlag = testMap.getHeight(new Vector3f(4, 0, 1));
+
         // Initialize game elements
-        RuleBasedBot bot = new RuleBasedBot(new Vector3f(1,yInitial,0),new Vector3f(4,yFlag,1));
-        Ball ball = new Ball(startingPosition);
-        Green green = new Green(flagPosition, flagRadius);
+        RuleBasedBot bot = new RuleBasedBot(new Vector3f(1, yInitial, 0), new Vector3f(4, yFlag, 1), testMap);
+        Ball ball = new Ball(bot.startingPosition);
+        Green green = new Green(bot.flagPosition, bot.flagRadius);
 
         // Find the best shot
-        Shot bestShot = findBestShot(ball, green);
+        Shot bestShot = bot.findBestShot(ball, green);
 
         // Output the best shot
         System.out.println("Best shot: " + bestShot);
     }
 
-    public static Shot findBestShot(Ball ball, Green green) {
+    // public Shot findBestShot(Ball ball, Green green) {
+    //     Shot bestShot = null;
+    //     Vector3f bestPosition = new Vector3f(startingPosition);
+    //     System.out.println("start");
+
+    //     boolean foundBestShot = false;
+
+    //     while (!isInHole(ball, green) && !foundBestShot) {
+    //         for (double velocityX = -5; velocityX <= 5; velocityX += 0.1) {
+    //             for (double velocityZ = -5; velocityZ <= 5; velocityZ += 0.1) {
+    //                 // Apply the velocities to the ball
+    //                 applyVelocities(ball, velocityX, velocityZ);
+
+    //                 // Simulate the ball's movement using the physics engine
+    //                 simulateBallMovement(ball);
+
+    //                 // Check if it's in the hole
+    //                 if (isInHole(ball, green)) {
+    //                     bestShot = new Shot(velocityX, velocityZ);
+    //                     bestPosition.set(ball.getPosition());
+    //                     foundBestShot = true;
+    //                     break;
+    //                 }
+
+    //                 // Reset ball position for the next shot
+    //                 ball.updatePosition(new Vector3f(startingPosition));
+    //             }
+    //             if (foundBestShot) break;
+    //         }
+    //         if (!foundBestShot) {
+    //             System.out.println("Can't make a hole in one");
+    //             ball.updatePosition(bestPosition);
+    //         }
+    //     }
+
+    //     return bestShot;
+    // }
+
+    public Shot findBestShot(Ball ball, Green green) {
+        double minDistance = Double.MAX_VALUE;
         Shot bestShot = null;
         Vector3f bestPosition = null;
         int count = 0;
@@ -50,10 +93,15 @@ public class RuleBasedBot {
                     simulateBallMovement(ball);
 
                     // Check if it's in hole
-                    if (isInHole(ball, green)) {
+                    if (ball.distanceToFlag(green) < minDistance) {
+                        if (isInHole(ball, green)) {
+                            bestShot = new Shot(velocityX, velocityZ);
+                            bestPosition = ball.getPosition();
+                            break;
+                        }
+                        minDistance = ball.distanceToFlag(green);
                         bestShot = new Shot(velocityX, velocityZ);
                         bestPosition = ball.getPosition();
-                        break;
                     }
 
                     // Reset ball position for the next shot
@@ -68,22 +116,18 @@ public class RuleBasedBot {
         return bestShot;
     }
 
-    // Placeholder for applying velocities to the ball
-    public static void applyVelocities(Ball ball, double velocityX, double velocityZ) {
+    public void applyVelocities(Ball ball, double velocityX, double velocityZ) {
         ball.setVelocityX(velocityX);
         ball.setVelocityZ(velocityZ);
     }
 
-    // Placeholder for simulating the ball's movement
-    public static void simulateBallMovement(Ball ball) {
-        // Testing with height map
-        double[] initialState = {startingPosition.x, startingPosition.z, ball.getVelocityX(), ball.getVelocityZ()}; // initialState = [x, z, vx, vz]
+    public void simulateBallMovement(Ball ball) {
+        double[] initialState = {startingPosition.x, startingPosition.z, ball.getVelocityX(), ball.getVelocityZ()};
         double h = 0.1; // Time step
-        //double totalTime = 5; // Total time
         PhysicsEngine engine = new PhysicsEngine(testMap, 0.08, 0.2, 0.1, 0.3);
-        Vector3f finalPosition = startingPosition;
+        Vector3f finalPosition = new Vector3f(startingPosition);
         for (int i = 0; i < 50; i++) {
-            finalPosition = engine.runImprovedEuler(initialState, h);
+            finalPosition = engine.runImprovedEuler(initialState, h).get(engine.runImprovedEuler(initialState, h).size()-1);
             initialState[0] = finalPosition.x;
             initialState[1] = finalPosition.z;
         }
@@ -91,9 +135,7 @@ public class RuleBasedBot {
         ball.updatePosition(finalPosition);
     }
 
-    // Placeholder for checking if the ball is in the hole
-    public static boolean isInHole(Ball ball, Green green) {
-        // Check if the ball is within the hole radius of the flag
+    public boolean isInHole(Ball ball, Green green) {
         double distanceToFlag = ball.distanceToFlag(green);
         return distanceToFlag <= green.getFlagRadius();
     }
