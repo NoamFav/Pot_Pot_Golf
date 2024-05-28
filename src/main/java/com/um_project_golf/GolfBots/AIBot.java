b1751b827fe352046570b9c0f67ec8df04d5421e
bruna <b.cavalcantilauro@student.maastrichtuniversity.nl>
2024-05-28 17:27:26 +0200

    New comments on AIBot and RuleBasedBot

--- src/main/java/com/um_project_golf/GolfBots/AIBot.java
+++ src/main/java/com/um_project_golf/GolfBots/AIBot.java
@@ -12,6 +12,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Class responsible for the logic behind the AI bot.
+ */
+
+
 @SuppressWarnings("FieldCanBeLocal")
 public class AIBot {
     private final Vector3f startingPosition;
@@ -36,11 +41,20 @@ public class AIBot {
         this.scene = scene;
     }
 
+    // Method returns the path the ball takes from its initial position until the last position found by the AI bot.
+    // It does so by using the Hill Climbing method: gets a random initial velocity, computes the shot's final position's
+    // distance to the flag and saves it. Changes slightly the velocity and compares this new shot with the old one, if there
+    // is an improvement, it takes the new shot and moves the ball to the end position of the shot. This process continues
+    // until the ball reaches the flag or there is no possible improvement to be made (for more details on this issue please
+    // consult the README).
     public List<List<Vector3f>> findBestShotUsingHillClimbing() {
+        int shotCounter = 0;
         Random random = new Random();
         List<List<Vector3f>> path = new ArrayList<>();
         Vector3f currentPosition = new Vector3f(startingPosition);
         Vector3f nextPosition;
+
+        System.out.println("Start");
 
         // Initialize with a random shot
         float velocityX = (float) (minVelocityX + (maxVelocityX - minVelocityX) * random.nextDouble());
@@ -49,30 +63,34 @@ public class AIBot {
         Shot currentShot = new Shot(velocity);
 
         Shot bestNeighbor = currentShot;
-        double bestNeighborDistance = evaluateShot(bestNeighbor);
-        ball.setPosition(currentPosition);
+        double bestNeighborDistance = evaluateShot(bestNeighbor); // Finds the distance from the flag of the hypothetical shot
+        ball.setPosition(currentPosition); // Resets position to initial position
 
 
         boolean improvement = true;
 
+        // Loops until there is no more possible improvement to be made
         while (improvement) {
             improvement = false;
             Shot neighborShot = currentShot;
 
-            double neighborDistance = evaluateShot(neighborShot);
+            double neighborDistance = evaluateShot(neighborShot); // New hypothetical shot
 
             nextPosition = new Vector3f(ball.getPosition());
             ball.setPosition(currentPosition);
 
+            // Compares distance of current shot with the distance of the last shot taken
             if(neighborDistance < bestNeighborDistance){
                 bestNeighborDistance = neighborDistance;
                 improvement = true;
-                System.out.println("improvement: " + nextPosition);
-                path.add(fullPath.get(nextPosition));
+                shotCounter++;
+                path.add(fullPath.get(nextPosition)); // adds the position to the path of points passed by
                 currentPosition = new Vector3f(nextPosition);
-                ball.setPosition(currentPosition);
+                ball.setPosition(currentPosition); // set the ball to the end position of the shot
+                System.out.println("Shot "+ shotCounter +".Improvement! Distance to flag: " + distanceToFlag());
+                // Checks if its in hole
                 if(isInHole()) {
-                    System.out.println("Is in hole");
+                    System.out.println("Ball is in hole! With " + shotCounter + " shots taken");
                     return path;
                 }
             } else{
@@ -87,10 +105,10 @@ public class AIBot {
                         Vector3f newVelocity = new Vector3f(newVelocityX, 0,newVelocityZ);
                         neighborShot = new Shot(newVelocity);
 
-                        neighborDistance = evaluateShot(neighborShot);
+                        neighborDistance = evaluateShot(neighborShot); // new hypothetical shot evaluated
 
-                        nextPosition = new Vector3f(ball.getPosition());
-                        ball.setPosition(currentPosition);
+                        nextPosition = new Vector3f(ball.getPosition()); // saves new hypothetical position
+                        ball.setPosition(currentPosition); // sets ball back to current position
 
 
                         if (neighborDistance < bestNeighborDistance) {
@@ -98,12 +116,14 @@ public class AIBot {
                             bestNeighborDistance = neighborDistance;
                             velocity = newVelocity;
                             improvement = true;
-                            System.out.println("improvement: " + nextPosition);
-                            path.add(fullPath.get(nextPosition));
+                            shotCounter++;
+                            path.add(fullPath.get(nextPosition)); // adds the position to the path of points passed by
                             currentPosition = new Vector3f(nextPosition);
-                            ball.setPosition(currentPosition);
+                            ball.setPosition(currentPosition); // sets the ball to the end position of the shot
+                            System.out.println("Shot "+ shotCounter +".Improvement! Distance to flag: " + distanceToFlag());
+                            // Checks if its in hole
                             if (isInHole()){
-                                System.out.println("Is in hole");
+                                System.out.println("Ball is in hole!With " + shotCounter + " shots taken");
                                 return path;
                             }
                             break;
@@ -121,16 +141,19 @@ public class AIBot {
         return path;
     }
 
+    // Method that evaluates the hypothetical shot
     public double evaluateShot(Shot shot) {
         applyVelocities(shot.velocity());
         simulateBallMovement();
         return distanceToFlag();
     }
 
+    // Updates velocity of the ball
     public void applyVelocities(Vector3f velocity) {
         velocityBall = velocity;
     }
 
+    // Simulates ball movement with the use of the Physics Engine
     public void simulateBallMovement() {
         fullPath = new HashMap<>();
         double[] initialState = {ball.getPosition().x, ball.getPosition().z, velocityBall.x, velocityBall.z};
@@ -146,11 +169,13 @@ public class AIBot {
         ball.setPosition(finalPosition.x,finalPosition.y,finalPosition.z);
     }
 
+    // Method that checks whether the ball is in hole
     public boolean isInHole() {
         double distanceToFlag = distanceToFlag();
         return distanceToFlag <= flagRadius;
     }
 
+    // Method that calculates the distance to the flag
     public double distanceToFlag() {
         double dx = flag.getPosition().x - ball.getPosition().x;
         double dy = flag.getPosition().y - ball.getPosition().y;
