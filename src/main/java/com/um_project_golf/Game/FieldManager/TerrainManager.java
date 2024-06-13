6feee1d8fbf9c84d2e3394eec2490bbbb986d13e
noamfavier <n.favier@student.maastrichtuniversity.nl>
2024-06-13 16:11:02 +0200

    isolated terrains in manager

---
+++ src/main/java/com/um_project_golf/Game/FieldManager/TerrainManager.java
@@ -0,0 +1,44 @@
+package com.um_project_golf.Game.FieldManager;
+
+import com.um_project_golf.Core.Entity.Terrain.BlendMapTerrain;
+import com.um_project_golf.Core.Entity.Terrain.Terrain;
+
+public class TerrainManager {
+    // Terrains
+    private Terrain terrain;
+    private Terrain ocean;
+    private BlendMapTerrain blendMapTerrain;
+    private BlendMapTerrain blueTerrain;
+
+    public Terrain getTerrain() {
+        return terrain;
+    }
+
+    public void setTerrain(Terrain terrain) {
+        this.terrain = terrain;
+    }
+
+    public Terrain getOcean() {
+        return ocean;
+    }
+
+    public void setOcean(Terrain ocean) {
+        this.ocean = ocean;
+    }
+
+    public BlendMapTerrain getBlendMapTerrain() {
+        return blendMapTerrain;
+    }
+
+    public void setBlendMapTerrain(BlendMapTerrain blendMapTerrain) {
+        this.blendMapTerrain = blendMapTerrain;
+    }
+
+    public BlendMapTerrain getBlueTerrain() {
+        return blueTerrain;
+    }
+
+    public void setBlueTerrain(BlendMapTerrain blueTerrain) {
+        this.blueTerrain = blueTerrain;
+    }
+}
