3a62a6219069c8610c24df4885956ca37d5a79ef
noamfavier <n.favier@student.maastrichtuniversity.nl>
2024-06-13 17:49:50 +0200

    isolated gamevar in manager

--- src/main/java/com/um_project_golf/Game/FieldManager/EntitiesManager.java
+++ src/main/java/com/um_project_golf/Game/FieldManager/EntitiesManager.java
@@ -24,111 +24,31 @@ public class EntitiesManager {
         this.treeHeights = new ArrayList<>();
     }
 
-    public List<Entity> getTrees() {
-        return trees;
-    }
-
-    public void addTree(Entity tree) {
-        this.trees.add(tree);
-    }
-
-    public void clearTrees() {
-        this.trees.clear();
-    }
-
-    public List<Float> getTreeHeights() {
-        return treeHeights;
-    }
-
-    public void addTreeHeight(Float height) {
-        this.treeHeights.add(height);
-    }
-
-    public void clearTreeHeights() {
-        this.treeHeights.clear();
-    }
-
-    public Entity getGolfBall() {
-        return golfBall;
-    }
-
-    public void setGolfBall(Entity golfBall) {
-        this.golfBall = golfBall;
-    }
-
-    public void setGolfBallPosition(Vector3f position) {
-        this.golfBall.setPosition(position);
-    }
-
-    public Entity getGolfBall2() {
-        return golfBall2;
-    }
-
-    public void setGolfBall2(Entity golfBall2) {
-        this.golfBall2 = golfBall2;
-    }
-
-    public void setGolfBall2Position(Vector3f position) {
-        this.golfBall2.setPosition(position);
-    }
-
-    public void updateCurrentBall(boolean isPlayer1Turn) {
-        currentBall = isPlayer1Turn ? golfBall : golfBall2;
-    }
-
-    public Entity getCurrentBall() {
-        return currentBall;
-    }
-
-    public void setCurrentBall(Entity currentBall) {
-        this.currentBall = currentBall;
-    }
-
-    public void setCurrentBallPosition(Vector3f position) {
-        this.currentBall.setPosition(position);
-    }
-
-    public Entity getBotBall() {
-        return botBall;
-    }
-
-    public void setBotBall(Entity botBall) {
-        this.botBall = botBall;
-    }
-
-    public Entity getAiBotBall() {
-        return aiBotBall;
-    }
-
-    public void setAiBotBall(Entity aiBotBall) {
-        this.aiBotBall = aiBotBall;
-    }
-
-    public Entity getEndFlag() {
-        return endFlag;
-    }
-
-    public void setEndFlag(Entity endFlag) {
-        this.endFlag = endFlag;
-    }
-
-    public void setEndFlagPosition(Vector3f position) {
-        this.endFlag.setPosition(position);
-    }
-
-    public Entity getArrowEntity() {
-        return arrowEntity;
-    }
-
-    public void setArrowEntity(Entity arrowEntity) {
-        this.arrowEntity = arrowEntity;
-    }
-
-    public void setArrowEntityPosition(Vector3f position) {
-        this.arrowEntity.setPosition(position);
-    }
-
-    public void setArrowEntityRotation(Vector3f rotation) {
-        this.arrowEntity.setRotation(rotation);
-    }
+    public List<Entity> getTrees() {return trees;}
+    public void addTree(Entity tree) {this.trees.add(tree);}
+    public void clearTrees() {this.trees.clear();}
+    public List<Float> getTreeHeights() {return treeHeights;}
+    public void addTreeHeight(Float height) {this.treeHeights.add(height);}
+    public void clearTreeHeights() {this.treeHeights.clear();}
+    public Entity getGolfBall() {return golfBall;}
+    public void setGolfBall(Entity golfBall) {this.golfBall = golfBall;}
+    public void setGolfBallPosition(Vector3f position) {this.golfBall.setPosition(position);}
+    public Entity getGolfBall2() {return golfBall2;}
+    public void setGolfBall2(Entity golfBall2) {this.golfBall2 = golfBall2;}
+    public void setGolfBall2Position(Vector3f position) {this.golfBall2.setPosition(position);}
+    public void updateCurrentBall(boolean isPlayer1Turn) {currentBall = isPlayer1Turn ? golfBall : golfBall2;}
+    public Entity getCurrentBall() {return currentBall;}
+    public void setCurrentBall(Entity currentBall) {this.currentBall = currentBall;}
+    public void setCurrentBallPosition(Vector3f position) {this.currentBall.setPosition(position);}
+    public Entity getBotBall() {return botBall;}
+    public void setBotBall(Entity botBall) {this.botBall = botBall;}
+    public Entity getAiBotBall() {return aiBotBall;}
+    public void setAiBotBall(Entity aiBotBall) {this.aiBotBall = aiBotBall;}
+    public Entity getEndFlag() {return endFlag;}
+    public void setEndFlag(Entity endFlag) {this.endFlag = endFlag;}
+    public void setEndFlagPosition(Vector3f position) {this.endFlag.setPosition(position);}
+    public Entity getArrowEntity() {return arrowEntity;}
+    public void setArrowEntity(Entity arrowEntity) {this.arrowEntity = arrowEntity;}
+    public void setArrowEntityPosition(Vector3f position) {this.arrowEntity.setPosition(position);}
+    public void setArrowEntityRotation(Vector3f rotation) {this.arrowEntity.setRotation(rotation);}
 }
--- src/main/java/com/um_project_golf/Game/FieldManager/ModelManager.java
+++ src/main/java/com/um_project_golf/Game/FieldManager/ModelManager.java
@@ -12,35 +12,12 @@ public class ModelManager {
     private List<Model> ball2;
     private List<Model> tree;
 
-    public List<Model> getBotBallModel() {
-        return botBallModel;
-    }
-
-    public void setBotBallModel(List<Model> botBallModel) {
-        this.botBallModel = botBallModel;
-    }
-
-    public List<Model> getAiBotBallModel() {
-        return aiBotBallModel;
-    }
-
-    public void setAiBotBallModel(List<Model> aiBotBallModel) {
-        this.aiBotBallModel = aiBotBallModel;
-    }
-
-    public List<Model> getBall2() {
-        return ball2;
-    }
-
-    public void setBall2(List<Model> ball2) {
-        this.ball2 = ball2;
-    }
-
-    public List<Model> getTree() {
-        return tree;
-    }
-
-    public void setTree(List<Model> tree) {
-        this.tree = tree;
-    }
+    public List<Model> getBotBallModel() {return botBallModel;}
+    public void setBotBallModel(List<Model> botBallModel) {this.botBallModel = botBallModel;}
+    public List<Model> getAiBotBallModel() {return aiBotBallModel;}
+    public void setAiBotBallModel(List<Model> aiBotBallModel) {this.aiBotBallModel = aiBotBallModel;}
+    public List<Model> getBall2() {return ball2;}
+    public void setBall2(List<Model> ball2) {this.ball2 = ball2;}
+    public List<Model> getTree() {return tree;}
+    public void setTree(List<Model> tree) {this.tree = tree;}
 }
