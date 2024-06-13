295aeb3802ab654704d57fdd85451b9b2f2b3416
noamfavier <n.favier@student.maastrichtuniversity.nl>
2024-06-13 17:49:50 +0200

    isolated gamevar in manager

--- src/main/java/com/um_project_golf/Game/FieldManager/TerrainManager.java
+++ src/main/java/com/um_project_golf/Game/FieldManager/TerrainManager.java
@@ -10,35 +10,12 @@ public class TerrainManager {
     private BlendMapTerrain blendMapTerrain;
     private BlendMapTerrain blueTerrain;
 
-    public Terrain getTerrain() {
-        return terrain;
-    }
-
-    public void setTerrain(Terrain terrain) {
-        this.terrain = terrain;
-    }
-
-    public Terrain getOcean() {
-        return ocean;
-    }
-
-    public void setOcean(Terrain ocean) {
-        this.ocean = ocean;
-    }
-
-    public BlendMapTerrain getBlendMapTerrain() {
-        return blendMapTerrain;
-    }
-
-    public void setBlendMapTerrain(BlendMapTerrain blendMapTerrain) {
-        this.blendMapTerrain = blendMapTerrain;
-    }
-
-    public BlendMapTerrain getBlueTerrain() {
-        return blueTerrain;
-    }
-
-    public void setBlueTerrain(BlendMapTerrain blueTerrain) {
-        this.blueTerrain = blueTerrain;
-    }
+    public Terrain getTerrain() {return terrain;}
+    public void setTerrain(Terrain terrain) {this.terrain = terrain;}
+    public Terrain getOcean() {return ocean;}
+    public void setOcean(Terrain ocean) {this.ocean = ocean;}
+    public BlendMapTerrain getBlendMapTerrain() {return blendMapTerrain;}
+    public void setBlendMapTerrain(BlendMapTerrain blendMapTerrain) {this.blendMapTerrain = blendMapTerrain;}
+    public BlendMapTerrain getBlueTerrain() {return blueTerrain;}
+    public void setBlueTerrain(BlendMapTerrain blueTerrain) {this.blueTerrain = blueTerrain;}
 }
