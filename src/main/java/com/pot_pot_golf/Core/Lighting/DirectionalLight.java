ac769a398ce3eddd03e33a438837cda368bb26f5
noamfav <noam.favier@icloud.com>
2024-09-12 18:56:39 +0200

    removed dead code

--- src/main/java/com/pot_pot_golf/Core/Lighting/DirectionalLight.java
+++ src/main/java/com/pot_pot_golf/Core/Lighting/DirectionalLight.java
@@ -8,7 +8,7 @@ import org.joml.Vector3f;
 public class DirectionalLight {
 
     private Vector3f color, direction; // color of the light, direction of the light
-    private float intensity; // intensity of the light
+    private final float intensity; // intensity of the light
 
     public DirectionalLight(Vector3f color, Vector3f direction, float intensity) {
         this.color = color;
@@ -34,9 +34,5 @@ public class DirectionalLight {
 
     public float getIntensity() {
         return intensity;
-    }
-
-    public void setIntensity(float intensity) {
-        this.intensity = intensity;
     }
 }
