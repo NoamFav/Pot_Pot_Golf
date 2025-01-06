c31cdb4ed26cfeb3c362db7550945ad3a7b2729d
NoamFav <95364866+NoamFav@users.noreply.github.com>
2025-01-06 19:10:04 +0100

    removed target

--- src/main/java/com/pot_pot_golf/Game/GameUtils/FieldManager/ModelManager.java
+++ src/main/java/com/pot_pot_golf/Game/GameUtils/FieldManager/ModelManager.java
@@ -5,9 +5,8 @@ import com.pot_pot_golf.Core.Entity.Model;
 import java.util.List;
 
 /**
- * The model manager class.
- * This class is responsible for managing the models of the game.
- * Stores the models of the game.
+ * The model manager class. This class is responsible for managing the models of the game. Stores
+ * the models of the game.
  */
 public class ModelManager {
 
@@ -17,12 +16,35 @@ public class ModelManager {
     private List<Model> ball2;
     private List<Model> tree;
 
-    public List<Model> getBotBallModel() {return botBallModel;}
-    public void setBotBallModel(List<Model> botBallModel) {this.botBallModel = botBallModel;}
-    public List<Model> getAiBotBallModel() {return aiBotBallModel;}
-    public void setAiBotBallModel(List<Model> aiBotBallModel) {this.aiBotBallModel = aiBotBallModel;}
-    public List<Model> getBall2() {return ball2;}
-    public void setBall2(List<Model> ball2) {this.ball2 = ball2;}
-    public List<Model> getTree() {return tree;}
-    public void setTree(List<Model> tree) {this.tree = tree;}
+    public List<Model> getBotBallModel() {
+        return botBallModel;
+    }
+
+    public void setBotBallModel(List<Model> botBallModel) {
+        this.botBallModel = botBallModel;
+    }
+
+    public List<Model> getAiBotBallModel() {
+        return aiBotBallModel;
+    }
+
+    public void setAiBotBallModel(List<Model> aiBotBallModel) {
+        this.aiBotBallModel = aiBotBallModel;
+    }
+
+    public List<Model> getBall2() {
+        return ball2;
+    }
+
+    public void setBall2(List<Model> ball2) {
+        this.ball2 = ball2;
+    }
+
+    public List<Model> getTree() {
+        return tree;
+    }
+
+    public void setTree(List<Model> tree) {
+        this.tree = tree;
+    }
 }
